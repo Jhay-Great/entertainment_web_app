d99924e42cb312da546721318131a5df69f539d6
Jhay-Great <alemonai.jenef@gmail.com>
2024-09-24 13:32:39 +0000

    adds initial sass files and styles and initial component markup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchComponent } from './components/search/search.component';
+import { NavComponent } from "./components/nav/nav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
