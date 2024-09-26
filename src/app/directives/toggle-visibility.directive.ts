83ddccc9da8126710bca059425e277036ef3e300
Jhay-Great <alemonai.jenef@gmail.com>
2024-09-26 12:01:06 +0000

    adds custom toggle directive

--- src/app/directives/toggle-visibility.directive.ts
+++ src/app/directives/toggle-visibility.directive.ts
@@ -1,11 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appToggleVisibility]',
   standalone: true
 })
 export class ToggleVisibilityDirective {
+  @Input () set toggleVisibility(condition:boolean) {
+    if (condition) {
+      this.renderer.setStyle(this.el.nativeElement, 'display', 'block');
+    } else {
+      this.renderer.setStyle(this.el.nativeElement, 'display', 'none');
 
-  constructor() { }
+    }
+  }
+
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2,
+  ) { }
+
 
 }
