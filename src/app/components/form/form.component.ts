cab6b839b498b46927c81add2ca48ce5f3872018
Jhay-Great <alemonai.jenef@gmail.com>
2024-09-26 19:47:14 +0000

    feat: adds a custom validator to validate the password and confirm password values

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,28 +1,32 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AppService } from '../../services/app-service/app.service';
+import { FormBuilder, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 export class FormComponent implements OnInit {
   // @Output () display:EventEmitter<boolean> = new EventEmitter;
   isLoginActive:boolean = false;
+  form!:FormGroup;
 
   constructor (
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private appService: AppService,
+    private fb: FormBuilder,
   ) {};
 
   ngOnInit(): void {
 
     this.appService.setFormStatus(true);
 
+    // gets current route path
     this.activatedRoute.url.subscribe(
       (UrlSegment) => {
         console.log('current url: ', UrlSegment[0].path);
@@ -33,11 +37,25 @@ export class FormComponent implements OnInit {
           this.isLoginActive = false;
         }
       }
-    )
-    
+    );
 
-    // console.log(this.router.url);
-    // this.handleFormDisplay();
+    // build reactive form
+    // login form
+    if (this.isLoginActive) {
+      this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.min(8), Validators.max(20)]]
+      })
+      
+    } else {
+      this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.min(8), Validators.max(20)]],
+        confirmPassword: ['', [Validators.required, Validators.min(8), Validators.max(20)]]
+      })
+
+    }
+    
   }
 
   handleFormDisplay() {
