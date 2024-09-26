1fddb3bcb096a47d32454ccd3056abf77d224998
Jhay-Great <alemonai.jenef@gmail.com>
2024-09-26 11:32:11 +0000

    adds untracked files, images and custom directive

---
+++ src/app/directives/toggle-visibility.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appToggleVisibility]',
+  standalone: true
+})
+export class ToggleVisibilityDirective {
+
+  constructor() { }
+
+}
