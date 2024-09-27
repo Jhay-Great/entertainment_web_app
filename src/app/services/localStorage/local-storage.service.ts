b5cee1a490661b47a4d62f1ad89facba460983ca
Jhay-Great <alemonai.jenef@gmail.com>
2024-09-27 09:25:37 +0000

    adds session storage class

--- src/app/services/localStorage/local-storage.service.ts
+++ src/app/services/localStorage/local-storage.service.ts
@@ -7,6 +7,7 @@ export class LocalStorageService {
 
   constructor() { }
 
+  // Local storage
   getItem (key:string) {
     return localStorage.getItem(key) 
   }
@@ -15,8 +16,33 @@ export class LocalStorageService {
     localStorage.setItem(key, item);
   }
 
-  clear () {
+  delete (key:string) {
+    localStorage.removeItem(key);
+  }
+
+  clearLocalStorage () {
     localStorage.clear();
   }
+  
+  
+}
+
+class SessionStorage {
+  
+  constructor () {};
+  
+  // Session storage
+  getItemFromSessionStorage (key:string) {
+    return sessionStorage.getItem(key);
+  }
+  
+  setItemToSessionStorage (key:string, item:any):void {
+    sessionStorage.setItem(key, item);
+  }
+  
+  deleteItemFromSessionStorage (key:string) {
+    sessionStorage.removeItem(key);
+  }
+
   
 }
