33037434712a7599d585dc843cc34b15e3c091df
Jhay-Great <alemonai.jenef@gmail.com>
2024-09-27 13:26:10 +0000

    feat: adds interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideState, provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { movieReducer } from './state/moive.reducer';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { MovieEffect } from './state/movie.effects';
+import { authenticationInterceptor } from './interceptors/authentication/authentication.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,7 +16,7 @@ export const appConfig: ApplicationConfig = {
     provideStore(),
     provideEffects(MovieEffect),
     provideState({ name: 'movies', reducer: movieReducer }),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authenticationInterceptor])),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
 ],
 };
