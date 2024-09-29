eeb58c6bde83bdd8bcc7911ee4f64272db61a0ed
Jhay-Great <alemonai.jenef@gmail.com>
2024-09-29 13:15:34 +0000

    feat: adds initial notification logic

--- src/app/components/notification/notification.component.ts
+++ src/app/components/notification/notification.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './notification.component.scss'
 })
 export class NotificationComponent {
+
+  @Input () notification!:{ status:boolean, message:string };
 
 }
