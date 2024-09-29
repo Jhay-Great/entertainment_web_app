8a9ab14e028952c49fa32debd20a67dc1bc43102
Jhay-Great <alemonai.jenef@gmail.com>
2024-09-29 09:10:28 +0000

    feat: adds not found page to bookmarks

--- src/app/components/bookmarks/bookmarks.component.ts
+++ src/app/components/bookmarks/bookmarks.component.ts
@@ -5,11 +5,12 @@ import { selectBookmarked } from '../../state/movie.selector';
 import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { MovieCardComponent } from "../movie-card/movie-card.component";
+import { NotFoundComponent } from "../not-found/not-found.component";
 
 @Component({
   selector: 'app-bookmarks',
   standalone: true,
-  imports: [AsyncPipe, MovieCardComponent],
+  imports: [AsyncPipe, MovieCardComponent, NotFoundComponent],
   templateUrl: './bookmarks.component.html',
   styleUrl: './bookmarks.component.scss'
 })
--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './not-found.component.scss'
 })
 export class NotFoundComponent {
+
+  @Input () message = 'results not found';
 
 }
